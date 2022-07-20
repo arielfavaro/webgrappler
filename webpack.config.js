cb82b5003cc96a2e3040d5a2358ca79a0ee1bfd6
Ariel Favaro <arieltainan@gmail.com>
2022-07-20 09:25:02 -0300

    indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'webgrappler.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      name: 'WebGrappler',
-      type: 'global',
-    }
-  },
+    entry: './src/index.js',
+    output: {
+        filename: 'webgrappler.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: {
+            name: 'WebGrappler',
+            type: 'global',
+        }
+    },
 };
-
